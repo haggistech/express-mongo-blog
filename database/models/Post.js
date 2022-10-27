9542342c898e74a98fcb5350709f52f16eb408ac
haggistech@gmail.com <haggistech@gmail.com>
2022-10-27 20:04:44 +0100

    first commit

---
+++ database/models/Post.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const PostSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  content: String,
+  username: String,
+  image: String,
+  createdAt: {
+    type: Date,
+    default: new Date()
+  },
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comments'
+  }]
+})
+
+const Post = mongoose.model('Post', PostSchema)
+
+module.exports = Post 
